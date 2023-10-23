4b78bb0d57cdca571f7be3d476b4fd2c5e2620ef
KosukeIsoogai <kouiso@ritmo.co.jp>
2023-10-23 23:59:56 +0900

    コミット

--- index.js
+++ index.js
@@ -1,134 +1,108 @@
-const axios = require('axios');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
+const { execSync } = require('child_process');
 
-// GitHub Actionsの入力値を取得する関数
 async function getActionInputs() {
-    // 入力値を取得して返す
     return {
         githubToken: core.getInput('github-token', { required: true }),
-        jiraBaseUrl: core.getInput('jira-base-url', { required: true }),
-        jiraEmail: core.getInput('jira-email', { required: true }),
-        jiraApiToken: core.getInput('jira-api-token', { required: true }),
+        jiraBaseUrl: process.env.JIRA_BASE_URL,
+        jiraEmail: process.env.JIRA_EMAIL,
+        jiraApiToken: process.env.JIRA_API_TOKEN,
         context: github.context
     };
 }
 
-class PRProcessor {
-    constructor(inputs) {
-        this.inputs = inputs;
-        this.octokit = github.getOctokit(inputs.githubToken);
-        this.context = inputs.context;
-    }
+async function assignPullRequestToCreator(octokit, context) {
+    const { issue, repo, payload } = context;
+    await octokit.rest.issues.addAssignees({
+        issue_number: issue.number,
+        owner: repo.owner,
+        repo: repo.repo,
+        assignees: [payload.pull_request.user.login]
+    });
+}
 
-    // PRのベースブランチが適切かを確認する関数
-    async ensureBaseBranchIsIntended() {
-        const intendedBase = this.context.payload.pull_request.base.ref;
-        const baseRef = this.context.payload.pull_request.base.ref;
-        
-        if (baseRef !== intendedBase) {
-            throw new Error("PR base branch is not the intended branch.");
+function determineLabelFromBranch(branchName) {
+    const prefixes = ['feature', 'bugfix', 'release', 'hotfix', 'support'];
+    return prefixes.find(prefix => branchName.startsWith(`${prefix}/`)) || '';
+}
+
+async function labelPullRequest(octokit, context, label) {
+    const { issue, repo } = context;
+    await octokit.rest.issues.addLabels({
+        owner: repo.owner,
+        repo: repo.repo,
+        issue_number: issue.number,
+        labels: [label]
+    });
+}
+
+async function setTitleWithJiraTask(octokit, context, branchName) {
+    const jiraID = branchName.match(/[A-Z]+-[0-9]+/);
+    if (!jiraID) return;
+
+    const jiraData = await fetchJiraData(jiraID[0]);
+    const updatedTitle = `[${branchName}] ${jiraData.fields.summary}`;
+    
+    await octokit.rest.pulls.update({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.issue.number,
+        title: updatedTitle
+    });
+}
+
+async function fetchJiraData(jiraID) {
+    return axios.get(`${process.env.JIRA_BASE_URL}/rest/api/2/issue/${jiraID}`, {
+        headers: {
+            'Authorization': `Basic ${Buffer.from(`${process.env.JIRA_EMAIL}:${process.env.JIRA_API_TOKEN}`).toString('base64')}`,
+            'Content-Type': 'application/json'
         }
+    }).then(response => response.data);
+}
+
+function ensureValidBaseBranch(context) {
+    const intendedBase = 'develop';
+    if (context.payload.pull_request.base.ref !== intendedBase) {
+        throw new Error("Error: PR base branch is not the intended branch.");
     }
+}
 
-    // PRがベースブランチと最新状態であるかを確認する関数
-    async checkIfPrIsUpToDate() {
-        const prNumber = this.context.payload.pull_request.number;
-        const repository = this.context.repo.repo;
-        const prInfo = JSON.parse(execSync(`gh pr view ${prNumber} --json base,head -R ${repository}`, { encoding: 'utf8' }));
-        
-        const headSha = prInfo.head.sha;
-        const baseSha = prInfo.base.sha;
-        const baseRef = this.context.payload.pull_request.base.ref;  // Get the baseRef directly from the context
-        const latestBaseSha = execSync(`git rev-parse origin/${baseRef}`, { encoding: 'utf8' }).trim();
-        
-        if (baseSha !== latestBaseSha) {
-            throw new Error("PR is not up-to-date with its base branch. Please update your branch.");
-        }
+function checkIfPullRequestIsUpToDate(octokit, context) {
+    const baseBranchSha = getLatestBaseBranchSha(context.payload.pull_request.base.ref);
+    const pullRequestBaseSha = fetchPullRequestBaseSha(octokit, context);
+    if (baseBranchSha !== pullRequestBaseSha) {
+        throw new Error("PR is not up-to-date with its base branch. Please update your branch.");
     }
+}
 
+function getLatestBaseBranchSha(baseRef) {
+    return execSync(`git rev-parse origin/${baseRef}`, { encoding: 'utf8' }).trim();
+}
 
-    // PRの内容をJiraの情報を元に更新する関数
-    async updatePrFromJira() {
-        const branchName = this.context.payload.pull_request.head.ref;
-        const currentDescription = this.context.payload.pull_request.body;
-        
-        const replacements = await this.extractJiraIdAndFetchDetails(branchName);
-        const newDescription = currentDescription
-            .replace(/\$JIRA_SBI/g, replacements["$JIRA_SBI"])
-            .replace(/\$JIRA_PBI/g, replacements["$JIRA_PBI"]);
-            
-        await this.updatePrDescription(newDescription);
-    }
-
-    extractJiraIdFromBranch(branchName) {
-        return branchName.match(/[A-Z]+-[0-9]+/);
-    }
-
-    // Jiraの詳細情報を取得する関数
-    async extractJiraIdAndFetchDetails(branchName) {
-        const branchSuffix = branchName.split('/')[1] || '';
-        let replacements = {
-            "$JIRA_SBI": branchSuffix,
-            "$JIRA_PBI": ''
-        };
-
-        const jiraID = this.extractJiraIdFromBranch(branchName);
-        if (jiraID) {
-            replacements = await this.fetchJiraDetails(jiraID[0]);
-        }
-
-        return replacements;
-    }
-
-    // Jiraのタスク情報を取得する関数
-    async fetchJiraDetails(jiraID) {
-        const jiraData = await getJiraTaskInfo(this.inputs.jiraBaseUrl, this.inputs.jiraEmail, this.inputs.jiraApiToken, jiraID);
-        let replacements = {
-            "$JIRA_SBI": '',
-            "$JIRA_PBI": ''
-        };
-
-        if (jiraData && jiraData.fields) {
-            if (jiraData.fields.parent) {
-                replacements["$JIRA_PBI"] = jiraData.fields.parent.key;
-            }
-            if (jiraData.fields.components && jiraData.fields.components.length > 0) {
-                const componentNames = jiraData.fields.components.map(comp => comp.name);
-                await this.addLabelsToPR(componentNames);
-            }
-        }
-        return replacements;
-    }
-
-    // PRの説明を更新する関数
-    async updatePrDescription(newDescription) {
-        await this.octokit.rest.pulls.update({
-            owner: this.context.repo.owner,
-            repo: this.context.repo.repo,
-            pull_number: this.context.payload.pull_request.number,
-            body: newDescription
-        });
-    }
-
-    // PRにラベルを追加する関数
-    async addLabelsToPR(labels) {
-        for (const label of labels) {
-            await this.octokit.rest.issues.addLabels({
-                owner: this.context.repo.owner,
-                repo: this.context.repo.repo,
-                issue_number: this.context.payload.pull_request.number,
-                labels: [label]
-            });
-        }
-    }
+async function fetchPullRequestBaseSha(octokit, context) {
+    const { repo, issue } = context;
+    const prInfo = await octokit.pulls.get({
+        owner: repo.owner,
+        repo: repo.repo,
+        pull_number: issue.number
+    });
+    return prInfo.data.base.sha;
 }
 
 async function run() {
     try {
         const inputs = await getActionInputs();
-        const processor = new PRProcessor(inputs);
-        await processor.process();
+        const octokit = github.getOctokit(inputs.githubToken);
+        
+        await assignPullRequestToCreator(octokit, inputs.context);
+        const label = determineLabelFromBranch(inputs.context.payload.pull_request.head.ref);
+        if (label) await labelPullRequest(octokit, inputs.context, label);
+        await setTitleWithJiraTask(octokit, inputs.context, branchName);
+        ensureValidBaseBranch(inputs.context);
+        checkIfPullRequestIsUpToDate(octokit, inputs.context);
+
     } catch (error) {
         core.setFailed(error.message);
     }
